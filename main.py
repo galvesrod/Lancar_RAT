5e7d0bb4447e84ca0f5f920113595d157d3c01d3
Gabriel <galves.rod@outlook.com>
2026-05-24 12:31:32 -0300

    Processo completo!

--- main.py
+++ main.py
@@ -4,12 +4,25 @@ from playwright.sync_api import Page, sync_playwright
 from dotenv import load_dotenv
 from time import sleep
 from datetime import date, datetime
+from pandas import pandas as pd, DataFrame
 
 MAIN_URL = "https://tasycorp-vivere.zion-srv.com/#/login"
 
-def formatar_data(data:str)->str:    
-    return datetime.strptime(data,'%d/%m/%Y %H:%M:%S').strftime('%d/%m/%Y %H:%M:%S')
+def formatar_data(data:str, format:str='%d/%m/%Y %H:%M:%S')->str:    
+    return datetime.strptime(data,format).strftime('%d/%m/%Y %H:%M:%S')
 
+def fechar_modal_operacao_abortada(page:Page) -> Page:
+    selector = 'div[role="alertdialog"].priority-dialog'
+    modal = page.locator(selector)
+    modal.locator('button#w-dialog-box-ok-button').click()
+    return page
+
+def fechar_modal_pr_atividade(page:Page) -> Page:
+    selector = 'div[role="alertdialog"] [w-code="1099967"]'
+    modal = page.locator(selector)
+    modal.locator('button.gwt-Button.btn-gray').click()
+    
+    return page
 
 def obter_painel(page:Page, wdbpanel:str|int) -> Page:
     locator = f'[dto-code="{str(wdbpanel)}"]'
@@ -28,6 +41,25 @@ def aguardar_carregamento(page:Page, locator:str='div.paginationtop'):
 def preecher_campo_texto(painel:Page, atributo:str, texto:str) -> None:
     painel.locator(f'[nmatributo="{atributo.upper()}"]').fill(texto)
 
+def preencher_radiobutton(painel:Page, atributo:str, valor:str) -> None:
+    locator = '[w-attr-name="IE_MODALIDADE"]'
+    group = painel.locator(locator)
+    group.locator(f'span#{valor}').click()
+
+def preencher_checkbox(painel:Page, atributo:str, valor:bool) -> None:
+    locator = f'[data-testid="{atributo}"]'
+    chk = painel.locator(locator)
+    is_checked = painel.locator(f'{locator} input')
+
+    if is_checked.is_checked() == valor:
+        print('Não foi necessário realizar a operação')
+        return   
+
+    chk.click()
+    sleep(1)
+    if is_checked.is_checked() != valor:
+        print("Não foi possível realizar a operação")
+
 def preecher_campo_numero(painel:Page, atributo:str, num:int) -> None:
     painel.locator(f'[nmatributo="{atributo.upper()}"]').fill(str(num))
 
@@ -35,10 +67,16 @@ def preecher_campo_data(painel:Page, atributo:str, data:str) -> None:
     seletor = f'[w-attr-name="{atributo.upper()}"] input'
     painel.locator(seletor).fill(data)
 
-def preecher_campo_lookup(painel:Page, atributo:str) -> None:
+def preecher_campo_lookup(painel:Page, atributo:str, texto:str) -> None:
     seletor = f'tasy-listbox[data-testid="{atributo.upper()}"]'
     painel.wait_for_selector(seletor)
-    painel.locator(seletor).select_option(value=60)
+    elemento  = painel.locator(seletor)
+    sleep(1)
+    elemento.click()
+    elemento.press("Enter")    
+    elemento.type(text="---")
+    sleep(1)
+    elemento.type(text=texto)
 
 def realizar_login(page:Page, usuario:str, senha:str)->Page:
     '''
@@ -175,25 +213,62 @@ def adicionar_registro(page:Page, wdbpanel:int|str) -> Page:
         return page
     return page
 
-def preecher_form_atividade(page:Page, wdbpanel:int|str) -> Page:
-    painel = obter_painel(page, wdbpanel)
+def salvar(page:Page, wdbpanel:int|str) -> Page:
+    painel = obter_painel(page=page, wdbpanel=wdbpanel)
+    painel.locator('tasy-wbutton[text="Salvar"]').click()
+    return page
 
-    preecher_campo_data(painel=painel, atributo="DT_INICIO_ATIV", data= formatar_data('12/05/2026 08:00:00'))
-    preecher_campo_data(painel=painel, atributo="DT_FIM_ATIV", data= formatar_data('12/05/2026 12:00:00'))
-    preecher_campo_lookup(painel=painel, atributo="IE_CLASSIFICACAO")
-    preecher_campo_numero(painel=painel, atributo="NR_SEQ_ETAPA_CRON",num=30631 )
-    preecher_campo_texto(painel=painel, atributo="ds_atividade",texto=f"""texto em \nmultiplas \tlinhas""" )
-    sleep(5)
+def preecher_form_atividade(page:Page, wdbpanel:int|str, dados: tuple) -> Page:
+    painel = obter_painel(page, wdbpanel)
+    dt_inicio = formatar_data( str(dados.DT_INICIO), '%Y-%m-%d %H:%M:%S' )
+    dt_fim = formatar_data( str(dados.DT_FIM), '%Y-%m-%d %H:%M:%S' )
+    atividade_id = dados.ATIVIDADE_ID
+    classificacao = dados.CLASSIFICACAO
+    atividade = dados.DESCRICAO
+    modalidade = dados.MODALIDADE
+
+    preecher_campo_data(painel=painel, atributo="DT_INICIO_ATIV", data=dt_inicio)
+    preecher_campo_data(painel=painel, atributo="DT_FIM_ATIV", data= dt_fim)
+    preecher_campo_numero(painel=painel, atributo="NR_SEQ_ETAPA_CRON",num=atividade_id )
+    preecher_campo_lookup(painel=painel, atributo="IE_CLASSIFICACAO",texto=classificacao)
+    preecher_campo_texto(painel=painel, atributo="ds_atividade",texto=atividade )
+    preencher_radiobutton(painel=painel, atributo="IE_MODALIDADE", valor=modalidade )
     return page
 
 def carregar_arquivo_dados():
+    return pd.read_excel(r'.\data\dados.xlsx')
     
-    pass
+def lancar_rats(page:Page, dados:DataFrame):
+    df = dados
+    projetos = df["PROJETO_ID"].unique()
 
+    for projeto in projetos:
+        # Filtrar projetos
+        page = filtrar_projeto(page=page, nr_seq_projeto=projeto)
+        page = abrir_projeto(page=page)
+        rats = df[df["PROJETO_ID"]  == projeto]["RAT_ID"].unique()
+        for rat in rats:
+            # Filtrar RAT
+            page = filtrar_rat(page=page, nr_seq_rat=rat)
+            # Abrir RAT
+            page = abrir_rat(page=page)        
+            atividades = df[ (df["PROJETO_ID"] == projeto) & (df["RAT_ID"] == rat) ]
+            for atividade in atividades.itertuples():                
+                page = adicionar_registro(page=page, wdbpanel=1094287)
+                page = preecher_form_atividade(page=page, wdbpanel=109287,dados=atividade)
+
+                sleep(1)
+                page = salvar(page=page, wdbpanel=1094287)
+                sleep(2)
+                page = fechar_modal_operacao_abortada(page=page)
+                sleep(2)
+                page = fechar_modal_pr_atividade(page=page)
+        
 
 
 def run():
     with sync_playwright() as p:
+        df = carregar_arquivo_dados()
         browser = p.chromium.launch(
             headless=False,channel="chrome",
             args=[
@@ -217,27 +292,12 @@ def run():
         
         # Abrir Função
         page = abrir_funcao(page=page, nome_funcao="Gestão de Projetos Philips")
-
-        # Filtrar Projeto
-        page = filtrar_projeto(page=page, nr_seq_projeto=694)
-
-        page = abrir_projeto(page=page)
-
-        # Filtrar RAT
-        page = filtrar_rat(page=page, nr_seq_rat=31873)
-
-        # Abrir RAT
-        page = abrir_rat(page=page)
-
-        page = adicionar_registro(page=page, wdbpanel=1094287)
-
-        page = preecher_form_atividade(page=page, wdbpanel=109287)
-
+        lancar_rats(page, df)
         page.wait_for_timeout(5000)
+        print('Fim do processo')
         browser.close()
 
 
 if __name__ == "__main__":
-    load_dotenv()
-    
+    load_dotenv()    
     run()
